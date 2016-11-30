bf58d7c962ec3e011b9c17bbd11224a3f2d286b5
gudmundur98 <gummibr98@gmail.com>
2016-11-30 11:03:38 +0000

    honnun á bolta og spaðanum komin

--- LokaverkefniFor/Form1.Designer.cs
+++ LokaverkefniFor/Form1.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Bakgrunnur = new System.Windows.Forms.Panel();
+            this.spaðinn = new System.Windows.Forms.PictureBox();
+            this.boltinn = new System.Windows.Forms.PictureBox();
+            this.Bakgrunnur.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.spaðinn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boltinn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Bakgrunnur
+            // 
+            this.Bakgrunnur.Controls.Add(this.boltinn);
+            this.Bakgrunnur.Controls.Add(this.spaðinn);
+            this.Bakgrunnur.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Bakgrunnur.Location = new System.Drawing.Point(0, 0);
+            this.Bakgrunnur.Name = "Bakgrunnur";
+            this.Bakgrunnur.Size = new System.Drawing.Size(1080, 601);
+            this.Bakgrunnur.TabIndex = 0;
+            // 
+            // spaðinn
+            // 
+            this.spaðinn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.spaðinn.BackColor = System.Drawing.Color.Black;
+            this.spaðinn.Location = new System.Drawing.Point(533, 541);
+            this.spaðinn.Name = "spaðinn";
+            this.spaðinn.Size = new System.Drawing.Size(185, 15);
+            this.spaðinn.TabIndex = 0;
+            this.spaðinn.TabStop = false;
+            // 
+            // boltinn
+            // 
+            this.boltinn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.boltinn.BackColor = System.Drawing.Color.Maroon;
+            this.boltinn.Location = new System.Drawing.Point(606, 378);
+            this.boltinn.Name = "boltinn";
+            this.boltinn.Size = new System.Drawing.Size(50, 50);
+            this.boltinn.TabIndex = 1;
+            this.boltinn.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1080, 601);
+            this.Controls.Add(this.Bakgrunnur);
+            this.Name = "Form1";
+            this.Text = "PingPong";
+            this.Bakgrunnur.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.spaðinn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boltinn)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Bakgrunnur;
+        private System.Windows.Forms.PictureBox boltinn;
+        private System.Windows.Forms.PictureBox spaðinn;
     }
 }
 
